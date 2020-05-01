4581510c66027315dbd47e70bc737de557c36b14
Garnik77 <garnop@mail.ru>
2020-05-01 23:27:33 +0400

    Updates

---
+++ precache-manifest.f73a10fd847209700ce69d4f7c3b0f30.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf02cb0d3f07018c9b80d5afe79bf44d",
+    "url": "/React-Redux/index.html"
+  },
+  {
+    "revision": "5d693ada8a28e8de0140",
+    "url": "/React-Redux/static/css/3.67a35e77.chunk.css"
+  },
+  {
+    "revision": "a8e736ce253b02864ab0",
+    "url": "/React-Redux/static/css/4.9a79a92b.chunk.css"
+  },
+  {
+    "revision": "248c9b3535620a0557b1",
+    "url": "/React-Redux/static/css/main.033c929a.chunk.css"
+  },
+  {
+    "revision": "ea555829c092121c1355",
+    "url": "/React-Redux/static/js/2.3bb9c129.chunk.js"
+  },
+  {
+    "revision": "5d693ada8a28e8de0140",
+    "url": "/React-Redux/static/js/3.14cf9243.chunk.js"
+  },
+  {
+    "revision": "a8e736ce253b02864ab0",
+    "url": "/React-Redux/static/js/4.8d0409b7.chunk.js"
+  },
+  {
+    "revision": "248c9b3535620a0557b1",
+    "url": "/React-Redux/static/js/main.1435bc76.chunk.js"
+  },
+  {
+    "revision": "cf63f968e08f1484d7ea",
+    "url": "/React-Redux/static/js/runtime-main.b15b04fb.js"
+  },
+  {
+    "revision": "3e17cba4d04d694b4157ce3861331619",
+    "url": "/React-Redux/static/media/6.3e17cba4.gif"
+  }
+]);
